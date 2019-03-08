0951ea788fcec2d3d276554e30dd11d39208d694
Phoebe Quincy <phoebequincy@gmail.com>
2019-03-08 11:32:37 -0700

    Updated ERD, added async componentDidMount to app.js.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
+
+
+
+
+
 class App extends Component {
-  render() {
+async componentDidMount(){
+  const response = await fetch('http://localhost:3000/acorns')
+          const json = await response.json()
+          let data = json.map((acorns,i) => {
+            return {
+              ...acorns,
+                // class: classArray[i],
+                // img: imgArray[i],
+                // overlay: spanClass[i]
+            }
+          })
+          console.log(data)
+          this.setState({ acorns:data })
+      }
+}
+
+
+    render() {
     return (
       <div className="App">
-        
+        // https://acorns4darkdays.herokuapp.com
       </div>
     );
   }
